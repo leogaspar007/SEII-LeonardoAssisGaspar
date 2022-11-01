14cea0ae6983942327785e641cff09f159a96607
Leonardo Assis Gaspar <lassis@aimirimsti.com.br>
2022-11-01 19:46:03 -0300

    Added the 'Semana04' files.

--- Semana04/exemplo4-3.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument("quadrado", help="Quadrado de um número", type=int)
-args = parser.parse_args()
-
-print(args.quadrado**2)
--- Semana04/exemplo4-5.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-import subprocess
-
-echo_data = subprocess.call("echo 'Hello world'", shell=True)
-
-# print(echo_data)
-
-subprocess.call(["ls", "/usr/", "-la"])
